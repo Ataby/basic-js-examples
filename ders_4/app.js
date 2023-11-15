e69b255369d7a0f2b85b506cbe17a4c5dc1778a6
m.said atabay <matabay200069@gmail.com>
2023-11-15 09:13:52 +0300

    if_Else & switch_case

--- ders_4/app.js
+++ ders_4/app.js
@@ -8,6 +8,14 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
+function hizBelirleme(){
+    let hiz = +prompt('hızınız giriniz: ');
+    let sonuc = hiz >=140 ? 'hız sınırını aştınız' : (hiz >=90 ? 'normal hızda gidiyorsunuz' : 'yavaş gidiyorsunuz, hızlanın');
+    console.log(sonuc);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 // function buyukSayi(){
     //     const a1 = +prompt(`ilk sayıyı giriniz:`);
     //     const a2 = +prompt(`ikinci sayıyı giriniz:`);
@@ -28,12 +36,46 @@
     
 // }
     ///////////////////////////////////////////////////////////////////////////////
+    
+    // function islemYap (){
+    //     let isaret = prompt(`4 işlem operatörlerinden bir tanesini sembol olarak giriniz: `);
+        
+    //     let s1 = +prompt(`1.sayıyı giriniz: `);
+    //     let s2 = +prompt(`2.sayıyı giriniz: `);
+        
+    //     console.log(`yapmak istediğiniz ${s1} ${isaret} ${s2} = ` + (s1(isaret),s2))
+    // }
 
-    function islemYap (){
-        let isaret = prompt(`4 işlem operatörlerinden bir tanesini sembol olarak giriniz: `);
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    // function askerlik(){
+    //     const yas=23;
+    //     const cinsiyet = 'erkek';
+    //     const saglik = true;
+        
+    //     const sart = saglik && (yas >18) && (cinsiyet == 'erkek');
+        
+    //     if( sart){
+    //         console.log('askerliğe uygundur');
+    //     }else { console.log('askerliğe uygun değildir');}
+    // }
+    
+    ///////////////////////////////////////////////////////////////////////////////
+ 
+function haftaGun(){   
+let gun = +prompt('haftanın kaçıncı günü ?')
 
-        let s1 = +prompt(`1.sayıyı giriniz: `);
-        let s2 = +prompt(`2.sayıyı giriniz: `);
+switch(gun){
+    case 1 : gun = 'pazartesi';break;
+    case 2 : gun = 'sali';break;
+    case 3 : gun = 'çarşamba';break;
+    case 4 : gun = 'perşembe';break;
+    case 5 : gun = 'cuma';break;
+    case 6 : gun = 'cumartesi';break;
+    case 7 : gun = 'pazar';break;
+    default: alert('Haftanın 7 günü vardı');break;
+}
+console.log(gun)
+}
 
-        console.log(`yapmak istediğiniz ${s1} ${isaret} ${s2} = ` + (s1(isaret),s2))
-    }
+

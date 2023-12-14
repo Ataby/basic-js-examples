1f0a0a86b5e027505e0a104874a8f3881ceb0173
Muhammed Atabay <matabay200069@gmail.com>
2023-12-14 17:46:47 +0300

    selectors types

--- ders_14/events.js
+++ ders_14/events.js
@@ -60,8 +60,42 @@ list.forEach ((li)=>{
 ////////////////////////////////////////////////////////////////////////
 
 //SAYFA TEKRAR YUKLENDIGINDE = ONLOAD()
-
+const buton=document.querySelector(".add-item #btn");
 window.onload = function(){
     document.querySelector('#input').focus(); 
     //SAYFA YENILENDIGINDE, INPUT BOX'A FOCUSLANSIN
+    buton.style.background = "black";
+    buton.style.color = "white";
+    buton.style.borderRadius= "9px";
+    buton.style.width="8rem";
+    buton.style.padding=".5rem";
+    
+    
 }
+////////////////////////////////////////////////////////////////////////
+
+
+// const liElement= document.getElementsByTagName("li");
+// console.log(liElement);
+
+// liElement[2].style.background="orange";
+// liElement[3].style.background="turquoise";
+// liElement.item(0).style.background="blue";
+// liElement.item(0).style.color="white";
+// liElement[4].innerText += " and Python"
+// //INNERTEXT SADECE GOZUKEN METNI GÖSTERİR. TEXTCONTENT GIZLI OLANLARI DA GOSTERIR.
+
+
+////////////////////////////////////////////////////////////////////////
+
+const myList = document.getElementsByClassName("list");
+const myListArray = Array.from(myList);
+myListArray.forEach((item)=>console.log(item.innerText));
+[...myList].forEach((item)=>console.log((item.textContent)));
+
+const itemh2 = document.querySelector(".item-list h2").style;
+itemh2.background="purple";
+itemh2.color = "white";
+const inputBox = document.querySelector("input[type='text']");
+inputBox.style.border = "2px solid red";
+console.log(document.querySelector("section ul li:nth-child(3)").innerText);
